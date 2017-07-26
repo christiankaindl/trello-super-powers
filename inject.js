dc07f6ec5e59afdae704b9e07c03e806cc4b5bf5
Christian Kaindl <crisscross.kaindl@outlook.de>
2017-07-26 18:19:49 +0200

    Fix function wrapper when there is no function

--- inject.js
+++ inject.js
@@ -1,8 +1,9 @@
 var input = document.createElement('input');
 
+// Add dummy element so we can use the clipboard function
 document.getElementsByTagName('body')[0].appendChild(input);
 
-(document.addEventListener('click', function(e) {
+document.addEventListener('click', function(e) {
         if(e.target.classList.contains('card-short-id')) {
                 e.stopImmediatePropagation();
                 e.preventDefault();
@@ -13,4 +14,4 @@ document.getElementsByTagName('body')[0].appendChild(input);
                 document.execCommand('copy');
                 console.log("Copied!");
         }
-}, true))();
+}, true);
