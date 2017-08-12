aeffe82ad8697b1275cd4ee8a92caf18343c34d5
Christian Kaindl <crisscross.kaindl@outlook.de>
2017-08-12 15:39:05 +0200

    Reorganize JavaScript code

--- inject.js
+++ inject.js
@@ -1,66 +1,89 @@
 "use strict";
 
-var input = document.createElement('input');
+/* Trello Super Powers add-on
 
-// Add dummy element so we can use the clipboard function
-document.getElementsByTagName('body')[0].appendChild(input);
+This script initializes these 3 features:
+1. copy card id on click
+2. compact mode
+3. resizable lists
+*/
 
-document.addEventListener('click', function(e) {
-        if(e.target.classList.contains('card-short-id')) {
-                e.stopImmediatePropagation();
-                e.preventDefault();
+var TSP = {
+	init() {
 
-                input.value = e.target.textContent;
-                input.select();
+		{ // Copy ID functionality
+			let input = document.createElement('input');
+			// Add dummy element so we can use the clipboard function
+			document.getElementsByTagName('body')[0].appendChild(input);
 
-                document.execCommand('copy');
-                console.log("Copied!");
-        }
-}, true);
+			document.addEventListener('click', function(e) {
+        			if(e.target.classList.contains('card-short-id')) {
+					e.stopImmediatePropagation();
+					e.preventDefault();
 
-var compactModeBtn = document.createElement('a');
+					input.value = e.target.textContent;
+					input.select();
 
-compactModeBtn.setAttribute('class', 'board-header-btn compact-mode-button');
-compactModeBtn.setAttribute('title', 'Toggle Compact Mode');
+					document.execCommand('copy');
+					console.info("Copied!");
+				}
+			}, true);
+		}
 
-{
-        let inner = document.createElement('span');
+		{ // Compact Mode functionality
+			let compactModeBtn = document.createElement('a');
 
-        inner.setAttribute('class', 'board-header-btn-text');
-        inner.textContent = "Compact Mode";
+			compactModeBtn.setAttribute('class', 'board-header-btn compact-mode-button');
+			compactModeBtn.setAttribute('title', 'Toggle Compact Mode');
 
-        compactModeBtn.appendChild(inner);
-}
+			{
+				let inner = document.createElement('span');
 
-compactModeBtn.addEventListener('click', function toggleCompactMode() {
-        // Toggle compactMode
-        document.getElementsByTagName('body')[0].classList.toggle('compact-mode');
-});
+				inner.setAttribute('class', 'board-header-btn-text');
+				inner.textContent = "Compact Mode";
 
-document.getElementById('permission-level').parentElement.appendChild(compactModeBtn);
+				compactModeBtn.appendChild(inner);
+			}
 
-var listWidth = 270;
-var styleElem = document.getElementsByTagName("head")[0].appendChild(document.createElement("style"));
-styleElem.setAttribute("id", "inserted-tsp-styles");
-styleElem.textContent = `.list-wrapper {width: ${listWidth}px}`;
-var resizeElem = document.createElement('div');
+			compactModeBtn.addEventListener('click', function toggleCompactMode() {
+				// Toggle compactMode
+				document.getElementsByTagName('body')[0].classList.toggle('compact-mode');
+			});
 
-resizeElem.setAttribute("class", "resize-element list-wrapper");
-resizeElem.addEventListener("mousedown", function (e) {
-	document.addEventListener("mousemove", attach);
-	document.addEventListener("mouseup", remove);
+			document.getElementById('permission-level').parentElement.appendChild(compactModeBtn);
+		}
 
-	function attach(e) {
-		document.getElementById("inserted-tsp-styles").textContent=`.list-wrapper {width: ${listWidth=listWidth+(e.movementX / 4)}px}`;
-		console.info(e.movementX);
-	}
-	function remove(e) {
-		console.log("invoked!");
-		//TODO: Save width to browser and apply it when page is reloaded
+		{ // Resizeable lists functionality
+			{
+				let styleElem = document.getElementsByTagName("head")[0].appendChild(document.createElement("style"));
 
+				styleElem.textContent = `.list-wrapper {width: ${TSP.listWidth}px}`;
+				styleElem = styleElem.setAttribute("id", "inserted-tsp-styles");
+			}
 
-		document.removeEventListener("mousemove", attach);
-		document.removeEventListener("mouseup", remove);
-	}
-});
-document.getElementById("board").appendChild(resizeElem);
+			var resizeElem = document.createElement('div');
+
+			resizeElem.setAttribute("class", "resize-element list-wrapper");
+			resizeElem.addEventListener("mousedown", function (e) {
+				document.addEventListener("mousemove", attach);
+				document.addEventListener("mouseup", remove);
+
+				function attach(e) {
+					document.getElementById("inserted-tsp-styles").textContent=`.list-wrapper {width: ${TSP.listWidth=TSP.listWidth+(e.movementX / 4)}px}`;
+					console.info(e.movementX);
+				}
+				function remove(e) {
+					//TODO: Save width to browser and apply it when page is reloaded
+
+					document.removeEventListener("mousemove", attach);
+					document.removeEventListener("mouseup", remove);
+				}
+			});
+			document.getElementById("board").appendChild(resizeElem);
+		}
+	},
+	listWidth: browser.storage.sync.get("listWidth").then(function(result) {return result;}, function(result) {TSP.listWidth = TSP.defaultListWidth;}) || TSP.defaultListWidth,
+	defaultListWidth: 270
+};
+
+TSP.init();
