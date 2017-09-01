b9a8625134ed9c429602f1a7ca3ad3dc1729038b
Christian Kaindl <crisscross.kaindl@outlook.de>
2017-09-01 19:13:46 +0200

    Version 0.6: Resizable lists and fixes

---
+++ background.js
@@ -0,0 +1,54 @@
+'use strict';
+console.clear();
+
+try {
+	const trelloBoardURL = /\S+:\/\/\S*\.?trello\.com\/b\/\S+/;
+	var injectStatus = undefined;
+
+	function moveOn(e) {
+	console.log(e);
+		if (e.width)
+			return;
+
+		browser.storage.local.set({list: {width: 270}})
+		.then((e) => {console.log("TSP: Defaulting to 270 for listWidth now.")});
+	}
+
+	function onError(error) {
+		console.log(`Error: ${error}`);
+	}
+
+	async function urlCheck(id, updateReason, state) {
+		if ( !(state.url.match(trelloBoardURL)) )
+			return;
+
+		if ( injectStatus == 'pending' )
+			return;
+
+		injectStatus = "pending";
+
+		try {
+			await browser.tabs.insertCSS({file: "/enhancedStyles.css"});
+			await browser.tabs.executeScript({file: "/inject.js"});
+
+			injectStatus = "fullfilled";
+			console.log(`TSP: successfully injected 'inject.js' into ${state.url}`);
+		}
+		catch(e) {
+			injectStatus = "rejected";
+			console.error(`TSP Error. Could not inject 'inject.js' into ${state.url}: ` , e);
+		}
+	}
+
+	browser.storage.local.get("list")
+	.then(moveOn, onError);
+
+	/* Trello uses AJAX loading and because of this when a board is loaded from
+	within the UI (not from URL) 'inject.js' won't be fired. To overcome this we
+	listen for URL changes initTrelloBoardand fire inizializations manually */
+	browser.tabs.onUpdated.addListener(urlCheck);
+
+	console.log("Trello Super Powers WebExtension started successfully.");
+}
+catch(e) {console.error("TSP Error: \n", e);}
+
--- inject.js
+++ inject.js
@@ -1,116 +1,132 @@
 "use strict";
 
-/* Trello Super Powers add-on
-
-This script initializes these 3 features:
+/*
 1. copy card id on click
 2. compact mode
 3. resizable lists
 */
-// What is this? --> browser.extensionTypes.RunAt = "document_end";
 
-var TSP = {
-	init() {
-		var copyIdFeature = new Promise(function(resolve, reject) {
-			let input = document.createElement('input');
-			// Add dummy element so we can use the clipboard function
-			document.getElementsByTagName('body')[0].appendChild(input);
+(async function() {
+	var listWidth,
+		features = [];
 
-			document.addEventListener('click', function(e) {
-        			if(e.target.classList.contains('card-short-id')) {
-					e.stopImmediatePropagation();
-					e.preventDefault();
+	listWidth = ( await browser.storage.local.get() ).list.width || 270;
 
-					input.value = e.target.textContent;
-					input.select();
+	if ( document.getElementById("tsp-init") ) // Already inizialized
+		return;
 
-					document.execCommand('copy');
-					console.info("Copied!");
+	if ( !(document.getElementById("board")) ) // Document not yet ready
+		return;
+
+	/* COPY CARD ID */
+	features.push(new Promise(function(resolve, reject) {
+		let input = document.createElement('input');
+		// Add dummy element so we can use the clipboard functionality
+		document.getElementsByTagName('body')[0].appendChild(input);
+
+		document.addEventListener('click', function(e) {
+    			if(e.target.classList.contains('card-short-id')) {
+				e.stopImmediatePropagation();
+				e.preventDefault();
+
+				input.value = e.target.textContent;
+				input.select();
+
+				document.execCommand('copy');
+			}
+		}, true);
+
+		resolve();
+	}));
+
+	/* COMPACT MODE */
+	features.push(new Promise(function(resolve, reject) {
+
+		let compactModeBtn = document.createElement('a');
+
+		compactModeBtn.setAttribute('class', 'board-header-btn compact-mode-button');
+		compactModeBtn.setAttribute('title', 'Toggle Compact Mode');
+
+		{
+			let inner = document.createElement('span');
+
+			inner.setAttribute('class', 'board-header-btn-text');
+			inner.textContent = "Compact Mode";
+
+			compactModeBtn.appendChild(inner);
+		}
+
+		compactModeBtn.addEventListener('click', function toggleCompactMode() {
+			// Toggle compactMode
+			document.getElementsByTagName('body')[0].classList.toggle('compact-mode');
+		});
+
+		document.getElementById('permission-level').parentElement.appendChild(compactModeBtn);
+
+
+		resolve();
+	}));
+
+	/* RESIZABLE LISTS */
+	features.push(new Promise(function(resolve, reject) {
+
+		function createResizeElem() {
+			let resizeElem = document.createElement('div');
+
+			resizeElem.setAttribute("class", "resize-element");
+			resizeElem.addEventListener("mousedown", function (e) {
+				document.addEventListener("mousemove", attach);
+				document.addEventListener("mouseup", remove);
+
+				function attach(e) {
+					let styleId = document.getElementById("inserted-tsp-styles"),
+						currentWidth = ((listWidth+(e.movementX / 4) < 400)&&(listWidth+(e.movementX / 4) > 150))
+						? (listWidth=listWidth+(e.movementX / 4))
+						: listWidth;
+
+					styleId.textContent=`.list-wrapper {width: ${currentWidth}px}`;
+					console.info(e.movementX);
 				}
-			}, true);
-			resolve();
-		});
-		var compactModeFeature = new Promise(function(resolve, reject) {
-			let compactModeBtn = document.createElement('a');
+				function remove(e) {
+				console.log(listWidth)
+					browser.storage.local.set({list:{width: listWidth}})
+					.catch((e) => {console.error("TSP: Could not save 'listWidth' to browser sync storage.")});
 
-			compactModeBtn.setAttribute('class', 'board-header-btn compact-mode-button');
-			compactModeBtn.setAttribute('title', 'Toggle Compact Mode');
-
-			{
-				let inner = document.createElement('span');
-
-				inner.setAttribute('class', 'board-header-btn-text');
-				inner.textContent = "Compact Mode";
-
-				compactModeBtn.appendChild(inner);
-			}
-
-			compactModeBtn.addEventListener('click', function toggleCompactMode() {
-				// Toggle compactMode
-				document.getElementsByTagName('body')[0].classList.toggle('compact-mode');
+					document.removeEventListener("mousemove", attach);
+					document.removeEventListener("mouseup", remove);
+				}
 			});
 
-			document.getElementById('permission-level').parentElement.appendChild(compactModeBtn);
+			return resizeElem;
+		}
 
-			resolve();
-		});
-		var resizableListsFeature = new Promise(function(resolve, reject) {
-			function createResizeElem() {
-				let resizeElem = document.createElement('div');
+		if (!document.getElementById("inserted-tsp-styles")) {
+			let styleElem = document.getElementsByTagName("head")[0].appendChild(document.createElement("style"));
 
-				resizeElem.setAttribute("class", "resize-element list-wrapper");
-				resizeElem.addEventListener("mousedown", function (e) {
-					document.addEventListener("mousemove", attach);
-					document.addEventListener("mouseup", remove);
+			styleElem.textContent = `.list-wrapper {width: ${listWidth}px}`;
+			styleElem = styleElem.setAttribute("id", "inserted-tsp-styles");
+		}
 
-					function attach(e) {
-						let styleId = document.getElementById("inserted-tsp-styles");
-						styleId.textContent=`.list-wrapper {width: ${TSP.listWidth=TSP.listWidth+(e.movementX / 4)}px}`;
-						console.info(e.movementX);
-					}
-					function remove(e) {
-						//TODO: Save width to browser and apply it when page is reloaded
+		var lists = [];
 
-						document.removeEventListener("mousemove", attach);
-						document.removeEventListener("mouseup", remove);
-					}
-				});
-
-				return resizeElem;
-			}
-
-			{
-			  let styleElem = document.getElementsByTagName("head")[0].appendChild(document.createElement("style"));
-
-			  styleElem.textContent = `.list-wrapper {width: ${TSP.listWidth}px}`;
-			  styleElem = styleElem.setAttribute("id", "inserted-tsp-styles");
-			}
-
-
-			var lists = [];
-
-			document.getElementById("board").childNodes.forEach(function(listReference, i) {
-				lists[i] = listReference;
-			});
-
-			for (let i = 1; i < lists.length; i++) {
-
-				let resizeElem = createResizeElem();
-
-				lists[i].parentElement.insertBefore(resizeElem, lists[i]);
-			}
-
-			resolve();
+		document.getElementById("board")
+		.childNodes.forEach( (listReference, i) => {
+			lists[i] = listReference;
 		});
 
-		Promise.all([
-			copyIdFeature,
-			compactModeFeature,
-			resizableListsFeature
-		]).then(function success() {console.log("Initialized successfully")}, function failure(e) {console.log(":(", e)})
-	},
-	listWidth: browser.storage.sync.get("listWidth").then(function(result) {return result;}, function(errMsg) {TSP.listWidth = TSP.defaultListWidth;}) || TSP.defaultListWidth,
-	defaultListWidth: 270
-};
+		for ( let i = 1; i < lists.length; i++ ) {
+			let resizeElem = createResizeElem();
+			lists[i].parentElement.insertBefore(resizeElem, lists[i]);
+		}
 
-TSP.init();
+		resolve();
+	}));
+
+	Promise.all([...features])
+	.then(
+		function success() {
+			console.info("TSP: Injected script successfully.");
+			document.getElementsByClassName('compact-mode-button')[0].setAttribute("id", "tsp-init");
+		},
+		function failure(e) {console.error("TSP Error: Could not inject script: \n", e);});
+})();
