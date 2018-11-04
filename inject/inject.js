4fcf7b2a6a931b5a5a7c821c6b43d01f9fc1a819
Christian Kaindl <crisscross.kaindl@outlook.de>
2018-11-04 19:53:03 +0100

    Enforce StandardJS style rules

--- inject/inject.js
+++ inject/inject.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict'
 
 /*
 Inizialize Trello Super Powers features
@@ -9,11 +9,11 @@ Inizialize Trello Super Powers features
 - number of cards per list
 */
 
-function gracefullyInject(feature) {
+function gracefullyInject (feature) {
   /** Takes a funtion and injects it
    * TODO: add error handling logic
    */
-  feature();
+  feature()
 }
 
 /**
@@ -21,8 +21,8 @@ function gracefullyInject(feature) {
  *
  * @returns {object} An object with current settings.
  */
-async function getSettings() {
-  return await browser.storage.local.get();
+async function getSettings () {
+  return await browser.storage.local.get()
 }
 
 // NOTE: Ideally in the future, each feature has its own module
@@ -32,216 +32,216 @@ var features = {
    *
    * @returns {promise}
    */
-  id() {
-    return new Promise(function(resolve, reject) {
-      let input = document.createElement("input");
+  id () {
+    return new Promise(function (resolve, reject) {
+      let input = document.createElement('input')
 
       // Add dummy element so we can use the clipboard functionality
-      input.setAttribute('style', 'height: 0px;padding: 0px; margin: 0px; border: none;');
-      document.getElementsByTagName("body")[0].appendChild(input);
+      input.setAttribute('style', 'height: 0px;padding: 0px; margin: 0px; border: none;')
+      document.getElementsByTagName('body')[0].appendChild(input)
 
-      document.addEventListener("click", copyCardURL, true);
-      function copyCardURL(e) {
+      document.addEventListener('click', copyCardURL, true)
+      function copyCardURL (e) {
         // Don't move on if the element clicked is not an ID
-        if (!e.target.classList.contains("card-short-id")) return;
+        if (!e.target.classList.contains('card-short-id')) return
 
-        let elem;
+        let elem
 
-        e.stopImmediatePropagation();
-        e.preventDefault();
+        e.stopImmediatePropagation()
+        e.preventDefault()
 
-        elem = e.target;
+        elem = e.target
         // Traverse up the DOM to find the anchor element with the URL we want to copy.
         while (true) {
-          elem = elem.parentNode;
+          elem = elem.parentNode
 
-          if (elem.tagName !== "A") continue;
+          if (elem.tagName !== 'A') continue
 
-          input.value = elem.href;
-          input.select();
-          document.execCommand("copy");
-          break;
+          input.value = elem.href
+          input.select()
+          document.execCommand('copy')
+          break
         }
 
         // NOTE: Content Scripts do not have access to the notifications API. If a notification should be sent, we have to communicate with the background script using the messaging API
         browser.runtime.sendMessage({
-          type: "notification",
-          message: browser.i18n.getMessage("injectIdNotificationBody")
-        });
+          type: 'notification',
+          message: browser.i18n.getMessage('injectIdNotificationBody')
+        })
       }
 
-      console.info("TSP: feature 'id' injected");
-      board.classList.add("TSP-id-enabled");
-      resolve();
-    });
+      console.info("TSP: feature 'id' injected")
+      board.classList.add('TSP-id-enabled')
+      resolve()
+    })
   },
   /**
    * Initializes Compact Mode feature
    *
    * @returns {promise}
    */
-  compact() {
-    return new Promise(async function(resolve, reject) {
-      function createCompactModeButton() {
-        let parent, child;
+  compact () {
+    return new Promise(async function (resolve, reject) {
+      function createCompactModeButton () {
+        let parent, child
 
-        parent = document.createElement("a");
-        parent.setAttribute("class", "board-header-btn compact-mode-button");
+        parent = document.createElement('a')
+        parent.setAttribute('class', 'board-header-btn compact-mode-button')
         // TODO: Internationalize this string!
-        parent.setAttribute("title", "Toggle Compact Mode");
+        parent.setAttribute('title', 'Toggle Compact Mode')
 
-        child = document.createElement("span");
+        child = document.createElement('span')
         // Apply Trello button class. NOTE: Provided by Trello itself
-        child.setAttribute("class", "board-header-btn-text");
-        child.textContent = browser.i18n.getMessage("settingsCompactTitle");
+        child.setAttribute('class', 'board-header-btn-text')
+        child.textContent = browser.i18n.getMessage('settingsCompactTitle')
 
-        parent.appendChild(child);
+        parent.appendChild(child)
 
-        return parent;
+        return parent
       }
 
-      function toggleCompactMode() {
+      function toggleCompactMode () {
         // Apply Compact Mode CSS
-        board.classList.toggle("compact-mode");
+        board.classList.toggle('compact-mode')
       }
 
-      let compactModeButton = createCompactModeButton();
+      let compactModeButton = createCompactModeButton()
 
-      compactModeButton.addEventListener("click", toggleCompactMode);
+      compactModeButton.addEventListener('click', toggleCompactMode)
 
       // Inject into page
       document
-        .getElementById("permission-level")
-        .parentElement.appendChild(compactModeButton);
+        .getElementById('permission-level')
+        .parentElement.appendChild(compactModeButton)
 
-      console.info("TSP: feature 'compact' injected");
-      board.classList.add("TSP-compact-enabled");
-      resolve();
-    });
+      console.info("TSP: feature 'compact' injected")
+      board.classList.add('TSP-compact-enabled')
+      resolve()
+    })
   },
   /**
    * Initializes Resizable Lists feature
    *
    * @returns {promise}
    */
-  resize() {
-    return new Promise(async function(resolve, reject) {
-      function createResizeElem() {
-        function attachListeners() {
-          document.addEventListener("mousemove", adjustSize);
-          document.addEventListener("mouseup", setSize);
+  resize () {
+    return new Promise(async function (resolve, reject) {
+      function createResizeElem () {
+        function attachListeners () {
+          document.addEventListener('mousemove', adjustSize)
+          document.addEventListener('mouseup', setSize)
         }
 
-        function adjustSize(e) {
-          let styleId = document.getElementById("inserted-tsp-styles"),
+        function adjustSize (e) {
+          let styleId = document.getElementById('inserted-tsp-styles'),
             currentWidth =
               listWidth + e.movementX / 4 < 400 &&
               listWidth + e.movementX / 4 > 150
                 ? (listWidth = listWidth + e.movementX / 4)
-                : listWidth;
+                : listWidth
 
-          styleId.textContent = `.list-wrapper {width: ${currentWidth}px}`;
+          styleId.textContent = `.list-wrapper {width: ${currentWidth}px}`
         }
 
-        function setSize(e) {
-          document.removeEventListener("mousemove", adjustSize);
-          document.removeEventListener("mouseup", setSize);
+        function setSize (e) {
+          document.removeEventListener('mousemove', adjustSize)
+          document.removeEventListener('mouseup', setSize)
 
           browser.storage.local.set({ list: { width: listWidth } }).catch(e => {
             console.error(
               "TSP: Could not save 'listWidth' to browser sync storage.",
               e
-            );
-          });
+            )
+          })
         }
 
-        let resizeElem = document.createElement("div");
+        let resizeElem = document.createElement('div')
 
-        resizeElem.setAttribute("class", "resize-element");
+        resizeElem.setAttribute('class', 'resize-element')
         // Listen for mouse down
-        resizeElem.addEventListener("mousedown", attachListeners);
+        resizeElem.addEventListener('mousedown', attachListeners)
 
-        return resizeElem;
+        return resizeElem
       }
 
       var listWidth,
-        lists = [];
+        lists = []
 
       try {
-        listWidth = (await browser.storage.local.get()).list.width;
+        listWidth = (await browser.storage.local.get()).list.width
       } catch (e) {
-        console.error("TSP Error: Could not get local storage", e);
-        listWidth = 270;
+        console.error('TSP Error: Could not get local storage', e)
+        listWidth = 270
       }
 
       // Insert a <style> element in the <head> if not already created
-      if (!document.getElementById("inserted-tsp-styles")) {
+      if (!document.getElementById('inserted-tsp-styles')) {
         let styleElem = document
-          .getElementsByTagName("head")[0]
-          .appendChild(document.createElement("style"));
+          .getElementsByTagName('head')[0]
+          .appendChild(document.createElement('style'))
 
-        styleElem.textContent = `.list-wrapper {width: ${listWidth}px}`;
-        styleElem.setAttribute("id", "inserted-tsp-styles");
+        styleElem.textContent = `.list-wrapper {width: ${listWidth}px}`
+        styleElem.setAttribute('id', 'inserted-tsp-styles')
       }
 
       // Make an array and put get references for the Trello lists
       document
-        .getElementById("board")
+        .getElementById('board')
         .childNodes.forEach((listReference, i) => {
-          lists[i] = listReference;
-        });
+          lists[i] = listReference
+        })
 
       // Cycle through the Trello lists in the DOM and append resize element after each
       for (let i = 1; i < lists.length; i++) {
-        let resizeElem = createResizeElem();
-        lists[i].parentElement.insertBefore(resizeElem, lists[i]);
+        let resizeElem = createResizeElem()
+        lists[i].parentElement.insertBefore(resizeElem, lists[i])
       }
 
-      console.log("TSP: feature 'resize' enabled");
-      resolve();
-    });
+      console.log("TSP: feature 'resize' enabled")
+      resolve()
+    })
   },
   /**
    * Initializes visible label text feature
    */
-  label() {
+  label () {
     // CSS only feature
-    console.info("TSP: feature 'label' injected");
-    board.classList.add("TSP-label-enabled");
+    console.info("TSP: feature 'label' injected")
+    board.classList.add('TSP-label-enabled')
   },
   /**
    * Initializes number of cards feature
    */
-  numberOfCards() {
+  numberOfCards () {
     // CSS only feature
-    console.info("TSP: feature 'numberOfCards' injected");
-    board.classList.add("TSP-numberOfCards-enabled");
+    console.info("TSP: feature 'numberOfCards' injected")
+    board.classList.add('TSP-numberOfCards-enabled')
   }
-};
+}
 
 /**
  * Initialize and inject all features.
  */
-async function initializeFeatures() {
+async function initializeFeatures () {
   // Already injected
-  if (document.getElementById("TSP-init")) return;
+  if (document.getElementById('TSP-init')) return
 
   // Trello board not yet ready
-  if (!board) return;
+  if (!board) return
 
-  let settings = await getSettings();
+  let settings = await getSettings()
 
   // IDEA: Maybe a loop that iterates the 'features' object would be even easier
-  if (settings.copyId) gracefullyInject(features.id);
-  if (settings.compactMode) gracefullyInject(features.compact);
-  if (settings.labelText) gracefullyInject(features.label);
-  if (settings.numberOfCards) gracefullyInject(features.numberOfCards);
+  if (settings.copyId) gracefullyInject(features.id)
+  if (settings.compactMode) gracefullyInject(features.compact)
+  if (settings.labelText) gracefullyInject(features.label)
+  if (settings.numberOfCards) gracefullyInject(features.numberOfCards)
   // Does not have a setting
-  gracefullyInject(features.resize);
+  gracefullyInject(features.resize)
 
-  let elem = document.createElement("span");
-  elem.setAttribute("id", "TSP-init");
-  board.appendChild(elem);
+  let elem = document.createElement('span')
+  elem.setAttribute('id', 'TSP-init')
+  board.appendChild(elem)
 }
 
 /**
@@ -250,20 +250,19 @@ async function initializeFeatures() {
  * @param {object} message Message from the Background Script.
  * @returns {Blob} Containing CSV data.
  */
-async function handleMessage(message) {
+async function handleMessage (message) {
   /*
     We use PapaParse here, a JSON to CSV library (and vica versa). PapaParse is
     injected by the background script, that is why we can use `Papa` namespace
   */
-  if (message.type === "fetch") {
-
+  if (message.type === 'fetch') {
     /**
     * Format raw JSON data from the Trello board to an object used later for csv
     * parsing
     *
     * @param {object} card Current card object from JSON data
     */
-    function formatData(card, i) {
+    function formatData (card, i) {
       // Bring the information in the right format
       let formattedCard = {
         name: card.name,
@@ -272,62 +271,62 @@ async function handleMessage(message) {
         idShort: card.idShort,
         description: card.desc,
         labels: (() => {
-          let labelNames = [];
+          let labelNames = []
           for (let j in card.labels) {
-            labelNames[j] = card.labels[j].name;
+            labelNames[j] = card.labels[j].name
           }
-          return labelNames.join(", ");
+          return labelNames.join(', ')
         })(),
         idList: card.idList,
         idBoard: card.idBoard,
         listName: (() => {
-          function matchListId(list) {
-            return list.id === card.idList;
+          function matchListId (list) {
+            return list.id === card.idList
           }
 
           // Find the list that the current card is in
-          let list = boardData.lists.find(matchListId);
-          return list.name;
+          let list = boardData.lists.find(matchListId)
+          return list.name
         })(),
         due: card.due
-      };
+      }
 
-      return formattedCard;
+      return formattedCard
     }
 
     let boardData,
       cardsData,
-      { delimiter = ";", includeArchived = false, tabUrl: boardUrl } = message;
+      { delimiter = ';', includeArchived = false, tabUrl: boardUrl } = message
 
     // Fetch JSON from current Trello board
     boardData = await fetch(`${boardUrl}.json`, {
-      credentials: "include"
-    });
-    boardData = await boardData.json();
+      credentials: 'include'
+    })
+    boardData = await boardData.json()
 
     // Pick only what we need and put it in `cardsData`
-    cardsData = boardData.cards;
+    cardsData = boardData.cards
     cardsData = cardsData
       .filter(card => (card.closed && includeArchived) || !card.closed)
-      .map(formatData);
+      .map(formatData)
 
     try {
-      cardsData = await JSON.stringify(cardsData);
+      cardsData = await JSON.stringify(cardsData)
 
       // `Papa.unparse` parses JSON data to CSV using the Papa Parse library
       // It also takes are of sanitization
       cardsData = Papa.unparse(cardsData, {
         delimiter: delimiter
-      });
+      })
     } catch (e) {
-      console.error("Could not parse JSON data: ", e);
+      console.error('Could not parse JSON data: ', e)
     }
 
-    return new Blob([cardsData], { type: "application/csv" });
+    return new Blob([cardsData], { type: 'application/csv' })
   }
 }
 
-var board = document.getElementById("board");
+var board = document.getElementById('board')
 
-browser.runtime.onMessage.addListener(handleMessage);
-initializeFeatures();
+browser.runtime.onMessage.addListener(handleMessage)
+initializeFeatures()
--- settings/options.js
+++ settings/options.js
@@ -1,39 +1,39 @@
-"use strict";
+'use strict'
 
 /**
  * Update storage with changed settings
  *
  * @param {object} e 'onchange' event object
  */
-async function updateStorage(e) {
-  var setting = e.target.getAttribute("name"),
-    value = e.target.checked;
-  await browser.storage.local.set({ [setting]: value });
+async function updateStorage (e) {
+  var setting = e.target.getAttribute('name'),
+    value = e.target.checked
+  await browser.storage.local.set({ [setting]: value })
 
   browser.notifications.create({
-    type: "basic",
-    title: browser.i18n.getMessage("settingsSavedNotificationTitle"),
-    message: browser.i18n.getMessage("settingsSavedNotificationBody"),
-    iconUrl: "../assets/logo.svg"
-  });
+    type: 'basic',
+    title: browser.i18n.getMessage('settingsSavedNotificationTitle'),
+    message: browser.i18n.getMessage('settingsSavedNotificationBody'),
+    iconUrl: '../assets/logo.svg'
+  })
 }
 
 /**
  * Initialize settings page. Apply saved settings from storage and add evnt listener to the input elements.
  */
-async function initialize() {
+async function initialize () {
   var settings = await browser.storage.local.get(),
-    inputs = document.getElementsByTagName("input"),
-    translateables = document.querySelectorAll("[data-translate]");
+    inputs = document.getElementsByTagName('input'),
+    translateables = document.querySelectorAll('[data-translate]')
 
   // Apply settings
   for (let i = 0; i < inputs.length; i++) {
-    inputs[i].checked = settings[inputs[i].getAttribute("name")];
-    inputs[i].addEventListener("change", updateStorage);
+    inputs[i].checked = settings[inputs[i].getAttribute('name')]
+    inputs[i].addEventListener('change', updateStorage)
   }
 
   // Apply localized/translated strings
-  translate();
+  translate()
 }
 
-initialize();
+initialize()
